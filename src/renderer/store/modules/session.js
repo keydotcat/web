fd22f3d330e28100b1e0cf00863e6da3d7673e4c
Adria Casajus <acasajus@key.cat>
2017-10-25 19:01:36 +0200

    Session store scaffolding

---
+++ src/renderer/store/modules/session.js
@@ -0,0 +1,30 @@
+const state = {
+  urlRoot: process.enc.NODE_ENV === 'developent' ? 'http://localhost:27312' : 'https://pen.key.cat/api',
+  sessionToken: ''
+}
+
+const mutations = {
+  SET_URL_ROOT (state, urlRoot) {
+    state.urlRoot = urlRoot
+  },
+  SET_SESSION_TOKEN (state, payload) {
+    state.sessionToken = payload
+  },
+  CLEAR_SESSION_TOKEN (state) {
+    state.sessionToken = ''
+  }
+}
+
+const actions = {
+  requestLogin (context, payload) {
+    this.http.post(this.state.urlRoot + '/login', payload).then((data) => {
+      console.log('hello', data)
+    })
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
