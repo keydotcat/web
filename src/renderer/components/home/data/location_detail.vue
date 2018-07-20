1d00f6c1d5cdf3ba6b843fce9c5d4ddb628a63b9
Adria Casajus <acasajus@key.cat>
2018-07-20 16:22:56 +0200

    Credentials definition

--- src/renderer/components/home/data/location_detail.vue
+++ src/renderer/components/home/data/location_detail.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title" v-if='!editName'>{{ location.name || 'Location name' }}
+      <h5 class="card-title" v-if='!editName'>{{ loc.name || 'Location name' }}
         <i class="fas fa-edit float-right" @click='editName=true'></i>
       </h5>
       <h5 class="card-title" v-if='editName'>
         <div class="input-group w-100">
-          <input type="text" v-model="location.name" class="form-control" placeholder="Location name" aria-label="name">
+          <input type="text" v-model="loc.name" class="form-control" placeholder="Location name" aria-label="name">
           <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button" @click="editName=false">Set name</button>
           </div>
         </div>
       </h5>
-      <h6 class="card-subtitle m-2 text-muted" v-if="location.id">{{location.id}}</h6>
+      <h6 class="card-subtitle m-2 text-muted" v-if="loc.id">{{loc.id}}</h6>
       <h6 class="card-subtitle m-2 text-muted">URLs </h6>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item url-group-item" v-for="(url,idurl) in location.urls">
+        <li class="list-group-item url-group-item" v-for="(url,idurl) in loc.urls">
           <span v-if="!isUrlBeingEdited(idurl)">{{url}} <i class="fas fa-edit float-right" @click='editUrl(idurl)'></i></span>
           <div v-if="isUrlBeingEdited(idurl)" class="input-group w-100">
             <input type="text" v-model="urlsInEditMode[idurl]" class="form-control" placeholder="Url" aria-label="name">
@@ -34,43 +34,69 @@
         </li>
         <li class="list-group-item url-group-item" @click="showNewUrl"><i class="fas fa-plus"></i> Add new url</li>
       </ul>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <h6 class="card-subtitle m-2 text-muted">Credentials</h6>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item url-group-item" v-for="(cred,idcred) in loc.creds">
+          <span v-if="!isCredBeingEdited(idcred)">to edit <i class="fas fa-edit float-right"></i></span>
+          <location-credential-editor v-bind:cred="cred" v-on:change="cred=$event" v-if="isCredBeingEdited(idcred)" class="w-100"></location-credential-editor>
+        </li>
+        <li class="list-group-item url-group-item" @click="addNewCredential"><i class="fas fa-plus"></i> Add new credential</li>
+      </ul>
+
+      <a href="#" class="card-link">Cancel</a>
+      <a href="#" class="card-link">Save</a>
     </div>
   </div>
 </template>
 
 <script>
+  import LocationCredentialEditor from '@/components/home/data/location_credential_editor'
+
   export default {
     name: 'location-detail',
+    components: {LocationCredentialEditor},
     props: {
       location: Object
     },
     data () {
+      var loc = {}
+      loc.name = this.location.name || ''
+      loc.urls = this.location.urls || []
+      loc.creds = this.location.credentials || []
+      loc.id = this.location.id
+      loc.labels = this.location.labels || []
       return {
+        loc: loc,
         editName: false,
         newUrl: '',
         showNewUrlInput: false,
-        urlsInEditMode: {}
+        urlsInEditMode: {},
+        showNewCredentialInput: false,
+        credsInEditMode: {}
       }
     },
     methods: {
+      isCredBeingEdited(idc) {
+        return idc in this.credsInEditMode
+      },
+      addNewCredential () {
+        this.loc.creds.push({})
+        var cid = this.loc.creds.length - 1
+        this.$set(this.loc.creds[cid], 'type', 'login')
+        this.$set(this.credsInEditMode, cid, true)
+      },
       saveUrl(idurl) {
-        this.location.urls[idurl] = this.urlsInEditMode[idurl]
+        this.loc.urls[idurl] = this.urlsInEditMode[idurl]
         this.$delete(this.urlsInEditMode, idurl)
       },
       isUrlBeingEdited(idurl) {
         return idurl in this.urlsInEditMode
       },
       editUrl (idurl) {
-        this.$set(this.urlsInEditMode, idurl, this.location.urls[idurl])
+        this.$set(this.urlsInEditMode, idurl, this.loc.urls[idurl])
       },
       addUrl () {
-        if( !('urls' in this.location) ) {
-          this.location.urls = []
-        }
-        this.location.urls.push( this.newUrl )
+        this.loc.urls.push( this.newUrl )
         this.showNewUrlInput = false
       },
       showNewUrl () {
