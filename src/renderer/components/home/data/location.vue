417269498b0047b1faadf375c86b92641c4abe35
Adria Casajus <acasajus@key.cat>
2018-08-03 17:47:44 +0200

    Improved location

--- src/renderer/components/home/data/location.vue
+++ src/renderer/components/home/data/location.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="w-100 p-2">
-    <div class="w-100 d-flex align-items-center">
-      <span class="h5 m-0 ml-1">{{secret.data.name}}</span>
+  <div class="w-100">
+    <div class="w-100 p-2 d-flex align-items-center">
+      <i @click="toggleCreds" v-if="!displayCreds" class="fas fa-caret-right"></i>
+      <i @click="toggleCreds" v-if="displayCreds" class="fas fa-caret-down"></i>
+      <span @click="toggleCreds" class="h5 m-0 ml-1">{{secret.data.name}}</span>
       <span class="badge badge-dark ml-1" v-for="label in secret.data.labels">{{label}}</span>
       <small class="text-muted ml-auto">{{$store.getters[`team.${secret.teamId}/name`]}}/{{secret.vaultId}}</small>
       <div class="dropdown ml-1">
@@ -12,53 +14,85 @@
         </div>
       </div>
     </div>
-    <div class="w-100 m-1 pl-5 d-flex align-items-center flex-wrap justify-content-start">
-      <div class="card" v-for="cred in secret.data.creds">
-        <div class="card-body p-2">
-          <span class="h6 mr-2"> {{cred.username}}</span>
-          <i class="fas fa-user mr-2" @click="copy(cred.username)" :data-toggle="'tooltip:'+vid" data-placement="top" title="Copy username to clipboard"></i>
-          <i class="fas fa-key mr-2" @click="copy(cred.password.toString())" :data-toggle="'tooltip:'+vid" data-placement="top" title="Copy password to clipboard"></i>
-          <input type="text" hidden ref="copyHelper" id="copyHelper"></input>
+    <div v-if="displayCreds" class="w-100 container border-top">
+      <div class="row">
+        <div class="col-sm-2 p-1 mr-2 text-right">Credentials</div>
+        <div class="col-sm border-left p-0">
+          <div v-for="cred in secret.data.creds" class="border-bottom p-1 d-flex justify-content-left lh-condensed">
+            <div>
+              <h6 class="my-0">{{cred.username}}</h6>
+              <small class="text-muted">{{cred.type}}</small>
+            </div>
+            <div class="flex-grow-1 d-flex justify-content-end">
+              <button class="btn btn-sm btn-outline-secondary align-self-center" @click="copy(cred.username)"><i class="fas fa-user"></i> Copy username</button>
+              <button class="btn btn-sm btn-outline-secondary align-self-center ml-2" @click="copy(cred.password.toString())"><i class="fas fa-key"></i> Copy password</button>
+              <input type="text" hidden ref="copyHelper" id="copyHelper"></input>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+    <div v-if="displayCreds && secret.data.urls.length > 0" class="w-100 container border-top">
+      <div class="row">
+        <div class="col-sm-2 p-1 mr-2 text-right">Urls</div>
+        <div class="col-sm border-left p-0">
+          <div v-for="url in secret.data.urls" class="border-bottom p-1 d-flex justify-content-left lh-condensed">
+            {{url}}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="displayCreds && secret.data.note.length > 0" class="w-100 container border-top">
+      <div class="row">
+        <div class="col-sm-2 p-1 mr-2 text-right">Notes</div>
+        <div class="col-sm border-left p-1">{{secret.data.note}}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import $ from 'jquery'
+import $ from 'jquery'
 
-  export default {
-    name: 'location',
-    props: {
-      secret: Object
+export default {
+  name: 'location',
+  props: {
+    secret: Object
+  },
+  data () {
+    return {
+      vid: null,
+      displayCreds: false
+    }
+  },
+  beforeMount () {
+    this.vid = this._uid
+  },
+  mounted () {
+    $(`[data-toggle="tooltip:${this.vid}"]`).tooltip()
+  },
+  methods: {
+    toggleCreds() {
+      this.displayCreds = !this.displayCreds
     },
-    data () {
-      return {
-        vid: null
-      }
+    editSecret() {
+      this.$router.push( `/home/data/location/${this.secret.fullId}` )
     },
-    beforeMount () {
-      this.vid = this._uid
+    deleteSecret() {
+      this.$emit('delete', this.secret)
     },
-    mounted () {
-      $(`[data-toggle="tooltip:${this.vid}"]`).tooltip()
-    },
-    methods: {
-      editSecret() {
-        this.$router.push( `/home/data/location/${this.secret.fullId}` )
-      },
-      deleteSecret() {
-        this.$emit('delete', this.secret)
-      },
-      copy(text) {
-        console.log('Copying', text)
-        var ref = this.$refs.copyHelper[0]
-        ref.value = text
-        ref.select()
-        document.execCommand('copy')
-        ref.value = ''
-      }
+    copy(text) {
+      console.log('Copying', text)
+      var ref = this.$refs.copyHelper[0]
+      ref.value = text
+      ref.select()
+      document.execCommand('copy')
+      ref.value = ''
     }
   }
+}
 </script>
+
+<style>
+div > .border-bottom:last-child { border-bottom: none!important;  }
+</style>
