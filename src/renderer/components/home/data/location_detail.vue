4980941b475be578d430ee86620a567a77a1ab00
Adria Casajus <acasajus@key.cat>
2018-08-06 19:31:43 +0200

    Added label editor

--- src/renderer/components/home/data/location_detail.vue
+++ src/renderer/components/home/data/location_detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title" @click="bEditingName=true" :class="{'text-danger':!isOkName}" v-if='!bEditingName'>{{ loc.name || 'Location name' }}
+     <h5 class="card-title" @click="bEditingName=true" :class="{'text-danger':!isOkName}" v-if='!bEditingName'>{{ loc.name || 'Location name' }}
         <i class="fas fa-edit float-right" @click='bEditingName=true'></i>
       </h5>
       <h5 class="card-title" v-if='bEditingName'>
@@ -12,48 +12,56 @@
           </div>
         </div>
       </h5>
-      <h6 class="card-subtitle m-2 text-muted" v-if="bSelectingVault">
-        <div class="form-group">
-          <label>Choose which vault to store the secret </label>
-          <select class="form-control form-control-sm" v-model="parentVault">
-            <option :value="{tid: vt.tid, vid: vt.vid}" v-for="vt in allVaults">{{vt.teamName}} / {{vt.vid}}</option>
-          </select>
+      <div class="row">
+        <div class="col">
+               <h6 class="card-subtitle m-2 text-muted" v-if="bSelectingVault">
+            <div class="form-group">
+              <label>Choose which vault to store the secret </label>
+              <select class="form-control form-control-sm" v-model="parentVault">
+                <option :value="{tid: vt.tid, vid: vt.vid}" v-for="vt in allVaults">{{vt.teamName}} / {{vt.vid}}</option>
+              </select>
+            </div>
+          </h6>
+          <h6 class="card-subtitle m-2 text-muted" v-if="!bSelectingVault">Vault {{$store.getters[`team.${parentVault.tid}/name`]}} / {{parentVault.vid}}</h6>
+          <h6 class="card-subtitle m-2">URLs </h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item url-group-item" v-for="(url,idurl) in loc.urls">
+              <span v-if="!isUrlBeingEdited(idurl)">{{url}} <i class="fas fa-edit float-right" @click='editUrl(idurl)'></i></span>
+              <div v-if="isUrlBeingEdited(idurl)" class="input-group w-100">
+                <input type="text" v-model="urlsInEditMode[idurl]" class="form-control" placeholder="Url" aria-label="name">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-secondary" type="button" @click="saveUrl(idurl)">Save</button>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item url-group-item" v-if="showNewUrlInput">
+              <div class="input-group w-100">
+                <input type="text" v-model="newUrl" class="form-control" placeholder="Url" aria-label="name">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-secondary" type="button" @click="addUrl">Add</button>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item url-group-item" @click="showNewUrl"><i class="fas fa-plus"></i> Add new url</li>
+          </ul>
+          <h6 class="card-subtitle m-2" :class="{'text-danger':!isOkCreds}">Credentials</h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item url-group-item" v-for="(cred,idcred) in loc.creds">
+              <span v-if="!isCredBeingEdited(idcred)">{{cred.type}}: {{cred.username}}<i @click="editCredential(idcred)" class="fas fa-edit float-right"></i></span>
+              <location-credential-editor :idcred="idcred" :cred="cred" v-on:change="credChangedCb($event)"
+                v-on:cancel="cancelCredChangeCb($event)" v-if="isCredBeingEdited(idcred)" class="w-100"></location-credential-editor>
+            </li>
+            <li class="list-group-item url-group-item" @click="addNewCredential"><i class="fas fa-plus"></i> Add new credential</li>
+          </ul>
+          <h6 class="card-subtitle m-2">Notes</h6>
+          <div class="form-group pl-2">
+            <textarea class="form-control" :rows="linesInNote" v-model="loc.note"></textarea>
+          </div>
         </div>
-      </h6>
-      <h6 class="card-subtitle m-2 text-muted" v-if="!bSelectingVault">Vault {{$store.getters[`team.${parentVault.tid}/name`]}} / {{parentVault.vid}}</h6>
-      <h6 class="card-subtitle m-2">URLs </h6>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item url-group-item" v-for="(url,idurl) in loc.urls">
-          <span v-if="!isUrlBeingEdited(idurl)">{{url}} <i class="fas fa-edit float-right" @click='editUrl(idurl)'></i></span>
-          <div v-if="isUrlBeingEdited(idurl)" class="input-group w-100">
-            <input type="text" v-model="urlsInEditMode[idurl]" class="form-control" placeholder="Url" aria-label="name">
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="button" @click="saveUrl(idurl)">Save</button>
-            </div>
-          </div>
-        </li>
-        <li class="list-group-item url-group-item" v-if="showNewUrlInput">
-          <div class="input-group w-100">
-            <input type="text" v-model="newUrl" class="form-control" placeholder="Url" aria-label="name">
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="button" @click="addUrl">Add</button>
-            </div>
-          </div>
-        </li>
-        <li class="list-group-item url-group-item" @click="showNewUrl"><i class="fas fa-plus"></i> Add new url</li>
-      </ul>
-      <h6 class="card-subtitle m-2" :class="{'text-danger':!isOkCreds}">Credentials</h6>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item url-group-item" v-for="(cred,idcred) in loc.creds">
-          <span v-if="!isCredBeingEdited(idcred)">{{cred.type}}: {{cred.username}}<i @click="editCredential(idcred)" class="fas fa-edit float-right"></i></span>
-          <location-credential-editor :idcred="idcred" :cred="cred" v-on:change="credChangedCb($event)"
-            v-on:cancel="cancelCredChangeCb($event)" v-if="isCredBeingEdited(idcred)" class="w-100"></location-credential-editor>
-        </li>
-        <li class="list-group-item url-group-item" @click="addNewCredential"><i class="fas fa-plus"></i> Add new credential</li>
-      </ul>
-      <h6 class="card-subtitle m-2">Notes</h6>
-      <div class="form-group pl-2">
-        <textarea class="form-control" :rows="linesInNote" v-model="loc.note"></textarea>
+        <div class="col col-sm-3 border-left">
+          <h6 class="card-subtitle m-2">Labels</h6>
+          <text-list-editor v-model="loc.labels"></text-list-editor>
+        </div>
       </div>
     </div>
     <div class="card-footer d-flex justify-content-end">
@@ -65,6 +73,7 @@
 
 <script>
 import LocationCredentialEditor from '@/components/home/data/location_credential_editor'
+import TextListEditor from '@/components/home/data/secret/text_list_editor'
 import SecretData from '@/classes/secret_data'
 
 function isObjEmpty(obj) {
@@ -78,7 +87,7 @@ function isObjEmpty(obj) {
 
 export default {
   name: 'location-detail',
-  components: {LocationCredentialEditor},
+  components: {LocationCredentialEditor, TextListEditor},
   props: {
     secret: Object
   },
